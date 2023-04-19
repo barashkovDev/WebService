591807e17a312290cd369be382bc96132223f7b4
barashkovDev <mikhail.e.barashkov@gmail.com>
2023-04-19 14:51:00 +0300

    Added a form for adding records

--- src/main/java/com/barashkov/demo/models/Post.java
+++ src/main/java/com/barashkov/demo/models/Post.java
@@ -8,6 +8,15 @@ import jakarta.persistence.Id;
 @Entity
 public class Post {
 
+    public Post() {
+    }
+
+    public Post(String title, String anons, String fullText) {
+        this.title = title;
+        this.anons = anons;
+        this.fullText = fullText;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
